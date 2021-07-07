94e24d5d926140dceed97655d05f170eecad5867
khdscor <khds_cor@naver.com>
2021-07-07 22:11:53 +0900

    환경세팅완료

---
+++ backend/sin/src/main/java/sin/sin/SinApplication.java
@@ -0,0 +1,13 @@
+package sin.sin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SinApplication.class, args);
+    }
+
+}
---
+++ backend/sin/src/test/java/sin/sin/SinApplicationTests.java
@@ -0,0 +1,13 @@
+package sin.sin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
