7b6a0d0f0f3f038b9f626775ebe665fd83b7103f
JM_CHOI_ <chlwjdals98@gmail.com>
2021-08-10 01:11:24 +0900

    feat: ProductReview sql 생성

--- backend/sin/src/test/java/sin/sin/sqlCreation/ProductListSQLTest.java
+++ backend/sin/src/test/java/sin/sin/sqlCreation/ProductListSQLTest.java
@@ -6,10 +6,7 @@ public class ProductListSQLTest {
 
     @Test
     public void test() throws Exception {
-//        //InStock
-//        System.out.print("INSERT INTO PRODUCT (NAME, PRICE, STATUS, DISCOUNT_PERCENT, CREATED_DATE) VALUES");
-//        System.out.println("('상품" + 0 + "'," + 0*1000 +",'InStock',"+0+","+"TO_CHAR(SYSTIMESTAMP, 'YYYY-MM-DD HH:MI:SS.FF3'));");
-
+        //InStock
         for(int i=0; i<=30; i++) {
             System.out.print("INSERT INTO PRODUCT (NAME, PRICE, STATUS, DISCOUNT_PERCENT, CREATED_DATE) VALUES");
             System.out.println("('상품" + i + "'," + i*1000 +",'InStock',"+i+","+"TO_CHAR(SYSTIMESTAMP-"+i+", 'YYYY-MM-DD HH:MI:SS.FF3'));");
