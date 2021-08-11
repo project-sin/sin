57a4cd7e5e9ed06477dadc2c110df85d35a5aa1e
JM_CHOI_ <chlwjdals98@gmail.com>
2021-08-11 21:09:01 +0900

    refactor: 신상품, 베스트, 알뜰쇼핑 컨트롤러 구현

--- backend/sin/src/main/java/sin/sin/controller/ProductListController.java
+++ backend/sin/src/main/java/sin/sin/controller/ProductListController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import sin.sin.domain.product.Product;
+import sin.sin.dto.ProductListResponse;
 import sin.sin.service.ProductListService;
 
 import java.util.List;
@@ -25,19 +26,23 @@ public class ProductListController {
 //        map.put("038", "신상품");
 //        map.put("029", "베스트");
 
-        //신상품 //이번달 출시 상품 조회
+        //신상품 //이번달 출시 상품 & 가장 최근 등록 상품순 조회
         if ("038".equals(category)) {
             List<Product> newProducts = productListService.newProductList();
             return ResponseEntity.ok().body(newProducts);
         }
 
-        //알뜰쇼핑 //할인율 높은 상품 조회
+        //베스트상품 //후기 많은 상품순 조회
+        else if("029".equals(category)){
+            List<ProductListResponse> bestProducts = productListService.BestProductList();
+            return ResponseEntity.ok().body(bestProducts);
+        }
+
+        //알뜰쇼핑 //할인율 높은 상품순 조회
         else if ("sale".equals(list)) {
             List<Product> cheapProducts = productListService.CheapProductList();
             return ResponseEntity.ok().body(cheapProducts);
         }
-
-        //TODO : 베스트 상품 조회
 
         return ResponseEntity.badRequest().body("잘못된 요청입니다");
     }
