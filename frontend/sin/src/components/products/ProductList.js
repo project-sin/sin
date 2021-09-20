e2f470d4daaf54b620e75714ac86581479b7eae3
khdscor <khds_cor@naver.com>
2021-09-20 21:59:10 +0900

    refactor: 디자인 수정

--- frontend/sin/src/components/products/ProductList.js
+++ frontend/sin/src/components/products/ProductList.js
@@ -6,34 +6,53 @@ import queryString from "query-string";
 const ProductsListWrap = styled.div`
   width: 1050px;
   margin: 0 auto;
-  border: 2px solid rgb(90, 155, 213);
   padding: 10px;
 `;
 const Container = styled.div`
+  position: relative;
   display: inline-block;
   width: 320px;
   margin:11px;
   height: 500px;
-  border: 2px solid rgb(90, 155, 213);
+  cursor: pointer;
 `;
 const ProductImg = styled.img`
-  display: inline-block;
+  position: absolute;
   width: 320px;
-  height: 400px;
+  height: 340px;
 `;
+
+const ProductDetails = styled.div`
+  position: absolute;
+  top: 350px;
+`;
+
 const ProductName = styled.div`
+  font-size: 25px;
 `;
+
 const ProductDiscountPercent = styled.div`
   display: inline-block;
+  font-size: 20px;
+  color: red;
+  font-weight: bold;
 `;
-const ProductPrevPrice = styled.div`
-  display: inline-block;
-  margin-left:10px;
-`;
+
 const ProductPrice = styled.div`
+  display: inline-block;
+  font-size: 20px;
+  margin-left:10px;
+  font-weight: bold;
 `;
+
+const ProductPrevPrice = styled.div`
+  color: gray;
+  text-decoration:line-through
+`;
+
 const ProductSummary = styled.div`
 `;
+
 const ProductList = (props) => {
   const category = queryString.parse(props.location.search).category;
   const list = queryString.parse(props.location.search).list;
@@ -49,11 +68,13 @@ const ProductList = (props) => {
   const productLists = products ? products.map((product)=>{
     return <Container>
              <ProductImg src= {product.imageUrl} />
-             <ProductName>{product.name}</ProductName>
-             <ProductDiscountPercent>{product.discountPercent}</ProductDiscountPercent>
-             <ProductPrevPrice>{product.price}</ProductPrevPrice>
-             <ProductPrice>할인적용가격</ProductPrice>
-             <ProductSummary>{product.contentSummary}</ProductSummary>
+             <ProductDetails>
+               <ProductName>{product.name}</ProductName>
+               <ProductDiscountPercent>{product.discountPercent}%</ProductDiscountPercent>
+               <ProductPrice>{product.price * (1-product.discountPercent/100)}원</ProductPrice>
+               <ProductPrevPrice>{product.price}원</ProductPrevPrice>
+               <ProductSummary>{product.contentSummary?product.contentSummary: "상품요약정보"}</ProductSummary>
+             </ProductDetails>
            </Container>;
   }) : "";
 
