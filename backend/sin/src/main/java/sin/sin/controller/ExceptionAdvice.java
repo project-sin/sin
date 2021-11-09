6ad1a30890628dcef7c4f7ea1f1caaf178f37649
khdscor <khds_cor@naver.com>
2021-11-09 13:13:20 +0900

    feat: 백엔드 회원가입시 이메일 아이디 중복일 경우 예외처리

---
+++ backend/sin/src/main/java/sin/sin/controller/ExceptionAdvice.java
@@ -0,0 +1,29 @@
+package sin.sin.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import sin.sin.dto.auth.ErrorResponse;
+import sin.sin.handler.exception.AlreadyExistedEmailException;
+import sin.sin.handler.exception.AlreadyExistedIdException;
+
+@RestControllerAdvice
+public class ExceptionAdvice {
+
+    @ExceptionHandler(AlreadyExistedEmailException.class)
+    public ResponseEntity<ErrorResponse> handleAlreadyExistedEmailException(
+        AlreadyExistedEmailException e) {
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler(AlreadyExistedIdException.class)
+    public ResponseEntity<ErrorResponse> handleAlreadyExistedIdException(
+        AlreadyExistedIdException e) {
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(new ErrorResponse(e.getMessage()));
+    }
+}
---
+++ backend/sin/src/main/java/sin/sin/dto/auth/ErrorResponse.java
@@ -0,0 +1,15 @@
+package sin.sin.dto.auth;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@Getter
+public class ErrorResponse {
+
+    private String message;
+}
