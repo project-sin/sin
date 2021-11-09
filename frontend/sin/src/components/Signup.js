feed13d8bbe0bc33639fb7a95baff3dade322f21
khdscor <khds_cor@naver.com>
2021-11-09 11:03:02 +0900

    feat: 프론트 회원가입 페이지 값 설정

--- frontend/sin/src/components/Signup.js
+++ frontend/sin/src/components/Signup.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 
 const Signupwrap = styled.div``
@@ -27,111 +27,184 @@ const Agreeinput = styled.input`margin-right: 10px;`
 const Agreelabel = styled.label`font-size: 14px; color: #4c4c4c;`
 
 const Signup = () => {
-    return (
-        <Signupwrap>
-            <Container>
-                <Row>
-                    <Signuptitlewrap><Signuptitle>회원가입</Signuptitle></Signuptitlewrap>
-                    <Table>
-                        <tr>
-                            <Th>아이디</Th>
-                            <td><Input type='text' placeholder="6자 이상의 영문 혹은 영문과 숫자를 조합" /></td>
-                        </tr>
-                        <tr>
-                            <Th>비밀번호</Th>
-                            <td><Input type='password' placeholder="비밀번호를 입력해주세요" /></td>
-                        </tr>
-                        <tr>
-                            <Th>비밀번호 확인</Th>
-                            <td><Input type='password' placeholder="비밀번호를 한번 더 입력해주세요" /></td>
-                        </tr>
-                        <tr>
-                            <Th>이름</Th>
-                            <td><Input type='text' placeholder="이름을 입력해주새요" /></td>
-                        </tr>
-                        <tr>
-                            <Th>이메일</Th>
-                            <td><Input type='text' placeholder="예: marketkurly@kurly.com" /></td>
-                        </tr>
-                        <tr>
-                            <Th>휴대폰</Th>
-                            <td><Input type='number' placeholder="숫자만 입력해주세요" /></td>
-                        </tr>
-                        <tr>
-                            <Th>주소</Th>
-                            <td><Addressbtn>주소검색</Addressbtn></td>
-                        </tr>
-                        <tr>
-                            <Th>성별</Th>
-                            <td>
-                                <Sexradio type='radio' name='sex' id='sex1' />
-                                <Sexlabel htmlFor='sex1'>남자</Sexlabel>
-                                <Sexradio type='radio' name='sex' id='sex2' />
-                                <Sexlabel htmlFor='sex2'>여자</Sexlabel>
-                                <Sexradio type='radio' name='sex' id='sex3' />
-                                <Sexlabel htmlFor='sex3'>선택안함</Sexlabel>
-                            </td>
-                        </tr>
-                        <tr>
-                            <Th>생년월일</Th>
-                            <Birthtd>
-                                <Birthinput type='number' placeholder='YYYY' />
-                                <Birthinput type='number' placeholder='MM' />
-                                <Birthinput type='number' placeholder='DD' />
-                            </Birthtd>
-                        </tr>
-                        <tr>
-                            <Th>추가입력 사항</Th>
-                            <td>
-                                <Sexradio type='radio' name='option' id='option1' />
-                                <Sexlabel htmlFor='option1'>추천인 아이디</Sexlabel>
-                                <Sexradio type='radio' name='option' id='option2' />
-                                <Sexlabel htmlFor='option2'>참여 이벤트명</Sexlabel>
-                            </td>
-                        </tr>
-                        <Signupbot>
-                            <Signupbotth>이용약관동의</Signupbotth>
-                            <Signupbottd>
-                                <Agreediv>
-                                    <Agreeinput type='checkbox' id='agree1' />
-                                    <Agreelabel htmlFor='agree1'>전체 동의합니다</Agreelabel>
-                                </Agreediv>
-                                <Agreediv>
-                                    <Agreeinput type='checkbox' id='agree2' />
-                                    <Agreelabel htmlFor='agree2'>이용약관 동의</Agreelabel>
-                                </Agreediv>
-                                <Agreediv>
-                                    <Agreeinput type='checkbox' id='agree3' />
-                                    <Agreelabel htmlFor='agree3'>개인정보 수집.이용 동의(필수)</Agreelabel>
-                                </Agreediv>
-                                <Agreediv>
-                                    <Agreeinput type='checkbox' id='agree4' />
-                                    <Agreelabel htmlFor='agree4'>개인정보 수집.이용 동의(선택)</Agreelabel>
-                                </Agreediv>
-                                <Agreediv>
-                                    <Agreeinput type='checkbox' id='agree5' />
-                                    <Agreelabel htmlFor='agree5'>무료배송,할인쿠폰 등 혜택/정보 수신 동의</Agreelabel>
-                                </Agreediv>
-                                <Agreediv>
-                                    <Agreeinput type='checkbox' id='agree6' />
-                                    <Agreelabel htmlFor='agree6'>sms&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</Agreelabel>
-                                    <Agreeinput type='checkbox' id='agree7' />
-                                    <Agreelabel htmlFor='agree7'>이메일</Agreelabel>
-                                </Agreediv>
-                                <Agreediv>
-                                    <Agreeinput type='checkbox' id='agree8' />
-                                    <Agreelabel htmlFor='agree8'>본인은 만 14세 이상입니다.</Agreelabel>
-                                </Agreediv>
-                            </Signupbottd>
-                        </Signupbot>
-                    </Table>
-                    <Singupfooter>
-                        <Signupbutton>가입하기</Signupbutton>
-                    </Singupfooter>
-                </Row>
-            </Container>
-        </Signupwrap>
-    )
+
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+  const [checkPassword, setCheckPassword] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [gender, setGender] = useState("");
+  const [birthYear, setBirthYear] = useState("");
+  const [birthMonth, setBirthMonth] = useState("");
+  const [birthDay, setBirthDay] = useState("");
+
+  const signup = () => {
+    if (id.length && password.length && checkPassword.length && name.length
+        && email.length && phoneNumber.length && gender.length
+        && birthYear.length && birthMonth.length && birthDay.length) {
+      if (password === checkPassword) {
+        //signUpApi(email, password, nickName, props.history);
+        if(birthYear >=0 && birthMonth >=1 && birthMonth <=12 && birthDay >= 1 && birthDay <=31){
+          alert("조건 충족");
+        }
+        alert("생년월일을 올바르게 작성해 주세요");
+      } else {
+        alert("비밀번호가 일치하지 않습니다");
+      }
+    } else {
+      alert("빈칸을 다 채워주세요");
+    }
+  };
+
+  return (
+      <Signupwrap>
+        <Container>
+          <Row>
+            <Signuptitlewrap><Signuptitle>회원가입</Signuptitle></Signuptitlewrap>
+            <Table>
+              <tr>
+                <Th>아이디</Th>
+                <td><Input type='text'
+                           value={id}
+                           onChange={e => setId(e.target.value)}
+                           placeholder="6자 이상의 영문 혹은 영문과 숫자를 조합"/></td>
+              </tr>
+              <tr>
+                <Th>비밀번호</Th>
+                <td><Input type='password'
+                           value={password}
+                           onChange={e => setPassword(e.target.value)}
+                           placeholder="비밀번호를 입력해주세요"/></td>
+              </tr>
+              <tr>
+                <Th>비밀번호 확인</Th>
+                <td><Input type='password'
+                           value={checkPassword}
+                           onChange={e => setCheckPassword(e.target.value)}
+                           placeholder="비밀번호를 한번 더 입력해주세요"/>
+                </td>
+              </tr>
+              <tr>
+                <Th>이름</Th>
+                <td><Input type='text'
+                           value={name}
+                           onChange={e => setName(e.target.value)}
+                           placeholder="이름을 입력해주새요"/></td>
+              </tr>
+              <tr>
+                <Th>이메일</Th>
+                <td><Input type='text'
+                           value={email}
+                           onChange={e => setEmail(e.target.value)}
+                           placeholder="예: marketkurly@kurly.com"/>
+                </td>
+              </tr>
+              <tr>
+                <Th>휴대폰</Th>
+                <td><Input type='number'
+                           value={phoneNumber}
+                           onChange={e => setPhoneNumber(e.target.value)}
+                           placeholder="숫자만 입력해주세요"/></td>
+              </tr>
+              <tr>
+                <Th>주소</Th>
+                <td><Addressbtn>주소검색</Addressbtn></td>
+              </tr>
+              <tr>
+                <Th>성별</Th>
+                <td>
+                  <Sexradio type='radio'
+                            name='sex'
+                            value={gender}
+                            onChange={e => setGender('man')}
+                            id='sex1'/>
+                  <Sexlabel htmlFor='sex1'>남자</Sexlabel>
+                  <Sexradio type='radio'
+                            name='sex'
+                            value={gender}
+                            onChange={e => setGender('woman')}
+                            id='sex2'/>
+                  <Sexlabel htmlFor='sex2'>여자</Sexlabel>
+                  <Sexradio type='radio'
+                            name='sex'
+                            value={gender}
+                            onChange={e => setGender('none')}
+                            id='sex3'/>
+                  <Sexlabel htmlFor='sex3'>선택안함</Sexlabel>
+                </td>
+              </tr>
+              <tr>
+                <Th>생년월일</Th>
+                <Birthtd>
+                  <Birthinput type='number'
+                              value={birthYear}
+                              onChange={e => setBirthYear(e.target.value)}
+                              placeholder='YYYY'/>
+                  <Birthinput type='number'
+                              value={birthMonth}
+                              onChange={e => setBirthMonth(e.target.value)}
+                              placeholder='MM'/>
+                  <Birthinput type='number'
+                              value={birthDay}
+                              onChange={e => setBirthDay(e.target.value)}
+                              placeholder='DD'/>
+                </Birthtd>
+              </tr>
+              <tr>
+                <Th>추가입력 사항</Th>
+                <td>
+                  <Sexradio type='radio' name='option' id='option1'/>
+                  <Sexlabel htmlFor='option1'>추천인 아이디</Sexlabel>
+                  <Sexradio type='radio' name='option' id='option2'/>
+                  <Sexlabel htmlFor='option2'>참여 이벤트명</Sexlabel>
+                </td>
+              </tr>
+              <Signupbot>
+                <Signupbotth>이용약관동의</Signupbotth>
+                <Signupbottd>
+                  <Agreediv>
+                    <Agreeinput type='checkbox' id='agree1'/>
+                    <Agreelabel htmlFor='agree1'>전체 동의합니다</Agreelabel>
+                  </Agreediv>
+                  <Agreediv>
+                    <Agreeinput type='checkbox' id='agree2'/>
+                    <Agreelabel htmlFor='agree2'>이용약관 동의</Agreelabel>
+                  </Agreediv>
+                  <Agreediv>
+                    <Agreeinput type='checkbox' id='agree3'/>
+                    <Agreelabel htmlFor='agree3'>개인정보 수집.이용 동의(필수)</Agreelabel>
+                  </Agreediv>
+                  <Agreediv>
+                    <Agreeinput type='checkbox' id='agree4'/>
+                    <Agreelabel htmlFor='agree4'>개인정보 수집.이용 동의(선택)</Agreelabel>
+                  </Agreediv>
+                  <Agreediv>
+                    <Agreeinput type='checkbox' id='agree5'/>
+                    <Agreelabel htmlFor='agree5'>무료배송,할인쿠폰 등 혜택/정보 수신
+                      동의</Agreelabel>
+                  </Agreediv>
+                  <Agreediv>
+                    <Agreeinput type='checkbox' id='agree6'/>
+                    <Agreelabel
+                        htmlFor='agree6'>sms&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</Agreelabel>
+                    <Agreeinput type='checkbox' id='agree7'/>
+                    <Agreelabel htmlFor='agree7'>이메일</Agreelabel>
+                  </Agreediv>
+                  <Agreediv>
+                    <Agreeinput type='checkbox' id='agree8'/>
+                    <Agreelabel htmlFor='agree8'>본인은 만 14세 이상입니다.</Agreelabel>
+                  </Agreediv>
+                </Signupbottd>
+              </Signupbot>
+            </Table>
+            <Singupfooter>
+              <Signupbutton onClick={() => signup()}>가입하기</Signupbutton>
+            </Singupfooter>
+          </Row>
+        </Container>
+      </Signupwrap>
+  )
 }
 
 export default Signup;
