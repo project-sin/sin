3c10d7a841bb0d5ac7a07786d70776f98a609390
khdscor <khds_cor@naver.com>
2021-12-01 21:23:02 +0900

    feat: 프론트 주소검색 페이지 구현(주소검색시 새창으로 열기)

--- frontend/sin/src/App.js
+++ frontend/sin/src/App.js
@@ -6,14 +6,15 @@ import Home from './components/Home';
 import Event from './components/event/Event';
 import Shoppinglist from './components/Shoppinglist';
 import Item from './components/Item';
-import Signup from './components/Signup';
-import Login from './components/Login';
+import Signup from './components/login/Signup';
+import Login from './components/login/Login';
 import Mypage from './components/Mypage';
 import Notice from './components/notice/Notice';
 import Footer from './components/Footer';
 import Noticepage from './components/notice/Noticepage';
 import NonProductEvent from "./components/event/NonProductEvent";
 import ProductList from "./components/products/ProductList";
+import PopupPostCode from "./components/login/address/PopupPostCode";
 
 const GlobalStyle = createGlobalStyle`
 * {padding: 0; margin: 0;}
@@ -51,7 +52,7 @@ function App() {
     <Route path='/shop/board/list' exact component={Notice} />
     <Route path='/shop/service/faq' exact component={Notice} />
     <Route path='/shop/board/view' exact component={Noticepage} />
-    <Footer />
+    <Route path='/shop/address' exact component={PopupPostCode} />
     </>
   );
 }
